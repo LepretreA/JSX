07c763008bf91b6931568a3a803d6400be995955
LepretreA <alex.lepretre@aol.com>
2023-12-19 10:32:41 +0100

    first commit

---
+++ JSXexo29/jsxexo29/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import LOGIN from './composant/login.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <LOGIN/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ JSXexo29/jsxexo29/src/composant/login.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+let user ={
+    Name: "M.Lagache",
+    Commentaire: "super génial",
+};
+
+const LOGIN=({variable})=>{
+
+    const login ='azertyuiop';
+    const MDP ='12345';
+
+    return (
+        <div>
+            <div>
+                login: {login}<br></br>
+                Mot De Passe : {MDP}
+
+            </div>
+            <div>
+                Je doit dire que {user.Name} est {user.Commentaire}.
+
+            </div>
+        </div>
+    );
+};
+
+export default LOGIN;
